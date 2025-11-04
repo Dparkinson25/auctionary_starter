d030b975e76ee53d7078bc33a3d78dfa2f8f1262
Dparkinson25 <Dparkinson25@icloud.com>
2025-11-04 09:52:05 +0000

    Develop user models for the Auctionary for functionality

--- app/controllers/user.server.controllers.js
+++ app/controllers/user.server.controllers.js
@@ -1,10 +1,34 @@
 // User Controller
 
+const { token } = require("morgan");
+
 // Placeholder for adding a new user
 const create_account = (req, res) => {
-    return res.sendStatus(500);
+    const salt = crypto.randomBytes(64);
+    const hash = getHash(req.body.password, salt);
+
+    const sql = "INSERT INTO users (first_name, last_name, email, password, salt) VALUES (?, ?, ?, ?)";
+    let values = [user.first_name, user.last_name, user.email, hash, salt];
+
+    db.run(sql, values, function(err) {
+        if (err) return done(err);
+        return res.sendStatus(200);
+    });
 };
 
+users.authenticateUser(req.body.email, req.body.password, (err, userId) => {
+  if (err) return res.Status(400)
+  users.getToken(userId, (err, token) => {
+    if(token){
+      return res.status(200).send({user_id: id, session_token: token})
+    } else {
+      users.setToken(id,(err, token) => {
+        if(err) return res.sendStatus(500)
+        return res.status(200).send({user_id: id, session_token: token})
+      })
+    }
+  })
+});
 // Placeholder for logging in a user
 const login = (req, res) => {
   res.sendStatus(500);
